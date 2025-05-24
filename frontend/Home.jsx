78bcd154c6db66f42e442ab46c137a3071700889
thedgarg31 <dgav3105@gmail.com>
2025-05-24 23:54:24 +0530

    Create Home.jsx

---
+++ frontend/Home.jsx
@@ -0,0 +1,12 @@
+// pages/Home.jsx
+import React from 'react';
+import StepsSection from '../components/Home/StepsSection';
+
+export default function Home() {
+  return (
+    <div>
+      {/* Other sections of the Home page */}
+      <StepsSection />
+    </div>
+  );
+}
