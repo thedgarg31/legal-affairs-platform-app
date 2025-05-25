a7e4d06c946fdc71fef6ab9f2c6d9e20cc337c0c
thedgarg31 <dgav3105@gmail.com>
2025-05-25 17:04:20 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,62 +1,30 @@
-// App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import { LanguageProvider } from './context/LanguageContext';
-import PrivateRoute from './components/common/PrivateRoute';
 import Navbar from './components/common/Navbar';
 import Footer from './components/common/Footer';
 import Home from './pages/Home';
 import Login from './pages/Login';
-import Register from './pages/Register';
-import VerifyEmail from './pages/VerifyEmail';
-import LawyerProfile from './pages/LawyerProfile';
-import FindLawyer from './pages/FindLawyer';
 import DocumentAnalysis from './pages/DocumentAnalysis';
-import Dashboard from './pages/Dashboard';
-import NotFound from './pages/NotFound';
-import './App.css'; // global styles
+import ChatWithPDF from './ChatWithPDF';
+import './App.css';
 
 function App() {
-    return (
-        <AuthProvider>
-            <LanguageProvider>
-                <Router>
-                    <div className="app-container">
-                        <Navbar />
-                        <main className="main-content">
-                            <Routes>
-                                <Route path="/" element={<Home />} />
-                                <Route path="/login" element={<Login />} />
-                                <Route path="/register" element={<Register />} />
-                                <Route path="/verify-email/:token" element={<VerifyEmail />} />
-                                <Route path="/find-lawyer" element={<FindLawyer />} />
-                                <Route path="/lawyer/:id" element={<LawyerProfile />} />
-                                <Route 
-                                    path="/dashboard" 
-                                    element={
-                                        <PrivateRoute>
-                                            <Dashboard />
-                                        </PrivateRoute>
-                                    } 
-                                />
-                                <Route 
-                                    path="/document-analysis" 
-                                    element={
-                                        <PrivateRoute>
-                                            <DocumentAnalysis />
-                                        </PrivateRoute>
-                                    } 
-                                />
-                                <Route path="*" element={<NotFound />} />
-                            </Routes>
-                        </main>
-                        <Footer />
-                    </div>
-                </Router>
-            </LanguageProvider>
-        </AuthProvider>
-    );
+  return (
+    <Router>
+      <div className="app-container">
+        <Navbar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/document-analysis" element={<DocumentAnalysis />} />
+            <Route path="/chat" element={<ChatWithPDF />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
